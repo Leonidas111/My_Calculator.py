cc1ce2c759f02224ec5a40d3d95ce5437dadcb6e
Leonidas111 <juanca930.jcdz@gmail.com>
2021-07-10 20:05:07 -0500

    Local EDIT: def add_operator(operator)

--- calculator.py
+++ calculator.py
@@ -9,11 +9,10 @@ def add_number(number):
     global i
     display.insert(i,number)
     i = (i+1)
-
-
-    
-
-
+def add_operator(operator):
+    global i
+    display.insert(i,operator)
+    i = (i+1)
 #BUTTONS COLUMN 0:
 Button(root,text="7",command=lambda:add_number(7)).grid(row=2,column=0,sticky=W+E)
 Button(root,text="4",command=lambda:add_number(4)).grid(row=3,column=0,sticky=W+E)
@@ -29,13 +28,13 @@ Button(root,text="6",command=lambda:add_number(6)).grid(row=3,column=2,sticky=W+
 Button(root,text="3",command=lambda:add_number(3)).grid(row=4,column=2,sticky=W+E)
 #BUTTONS COLUMN 3:
 Button(root,text="DEL").grid(row=2,column=3,sticky=W+E)
-Button(root,text="x").grid(row=3,column=3,sticky=W+E)
-Button(root,text="+").grid(row=4,column=3,sticky=W+E)
-Button(root,text="=").grid(row=5,column=3,columnspan=2,sticky=W+E)
+Button(root,text="x",command=lambda:add_operator("*")).grid(row=3,column=3,sticky=W+E)
+Button(root,text="+",command=lambda:add_operator("+")).grid(row=4,column=3,sticky=W+E)
+Button(root,text="=",command=lambda:add_operator("=")).grid(row=5,column=3,columnspan=2,sticky=W+E)
 #BUTTONS COLUMN 4:
 Button(root,text="AC").grid(row=2,column=4,sticky=W+E)
-Button(root,text="/").grid(row=3,column=4,sticky=W+E)
-Button(root,text="-").grid(row=4,column=4,sticky=W+E)
+Button(root,text="/",command=lambda:add_operator("/")).grid(row=3,column=4,sticky=W+E)
+Button(root,text="-",command=lambda:add_operator("-")).grid(row=4,column=4,sticky=W+E)
 
 
 root.mainloop()
